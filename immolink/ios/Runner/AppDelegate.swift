fe416bf8cc6cc5b6aac6e9b87c5fd53912b46050
Fabian Boni <58946541+FabianBoni@users.noreply.github.com>
2025-12-22 11:45:59 +0100

    update

--- immolink/ios/Runner/AppDelegate.swift
+++ immolink/ios/Runner/AppDelegate.swift
@@ -1,6 +1,7 @@
 import Flutter
 import UIKit
 import GoogleMaps
+import FirebaseCore
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,6 +9,9 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    if FirebaseApp.app() == nil {
+      FirebaseApp.configure()
+    }
     GMSServices.provideAPIKey("AIzaSyBn2DBnF5XDD-X4JkrT0XKDJSAZwydyNY4")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
